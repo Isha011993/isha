6a8025bbeb157ba8970120999210e2b4dc793a20
isha <ishabhatia0111@gmail.com>
2020-01-02 13:33:54 +0000

    first commit

---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void apiAutomation()
+	{
+		System.out.println("apiAutomation");
+	}
+	 
+	@Test
+	public void elementAPI()
+	{
+		System.out.println("elementAPI");
+	}
+	
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowswerAutomation()
+	{
+		System.out.println("BrowswerAutomation");
+	}
+	 
+	@Test
+	public void elementUI()
+	{
+		System.out.println("elementUI");
+	}
+
+}
